953e20ad99b992b065655610a401e930722dbf6b
aflahmod123 <78242333+aflahmod123@users.noreply.github.com>
2022-07-23 00:24:58 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function draw() {
   let asciiImage = "";
   for (let j = 0; j < video.height; j++) {
     for (let i = 0; i < video.width; i++) {
-      const pixelIndex = (i + j * video.width) * 4;
+      const pixelIndex = (i + j * video.width) * 2;
       const r = video.pixels[pixelIndex + 0];
       const g = video.pixels[pixelIndex + 1];
       const b = video.pixels[pixelIndex + 2];
